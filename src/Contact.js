c006204152957afff27678af5d1478bcd75ba5f0
Johnathan Niziol <johnathan.niziol@mitt.ca>
2020-05-28 08:48:10 -0500

    adding state

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact(props) {
+  return (
+    <li className='contact-list-item'>
+      <div className='contact-avatar' style={{backgroundImage: `url(${props.contact.avatarURL})`}}/>
+      <div className='contact-details'>
+        <p>{props.contact.name}</p>
+        <p>{props.contact.email}</p>
+      </div>
+      <button className='contact-remove' onClick={() => props.onRemoveContact(props.contact)}>
+        Remove
+      </button>
+    </li>
+  );
+}
+
+export default Contact
