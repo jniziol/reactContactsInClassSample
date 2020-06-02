b42d142628201181d576a4898423002615216975
Johnathan Niziol <johnathan.niziol@mitt.ca>
2020-06-02 14:49:01 -0500

    current state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
 import Contact from './Contact';
-import ContactsTop from './ContactsTop'
 
 class App extends React.Component {
-  state = {   
+  state = {
+    query: "",
     contacts: [
       {
         "id": "1",
@@ -35,7 +35,7 @@ class App extends React.Component {
 
   removeContact = contact => {
     this.setState(previousState => ({contacts: previousState.contacts.filter(stateContact => stateContact !== contact)}));
-  }  
+  }
 
   createContact = (name, email, avatar) => {
     this.setState(previousState => ({
@@ -46,17 +46,36 @@ class App extends React.Component {
       }]
     }))
   }
-  
+
+  updateQuery = e => {
+    this.setState({query: e.target.value})
+  }
+
   render = () => {
+    let contacts = [...this.state.contacts];
+
+    if (this.state.query) {
+      contacts = contacts.filter(contact => contact.name.includes(this.state.query));
+    }
+
     return (
       <div className='list-contacts'>
-        <ContactsTop createContact={this.createContact}/>
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Filter Contacts'
+            value={this.state.query}
+            onChange={this.updateQuery}
+          />
+          <a href="" className='add-contact' onClick={this.addContact} >Add Contact</a>
+        </div>
         <ol className='contacts-list'>
-          {this.state.contacts.map(contact => <Contact key={contact.id} contact={contact} onRemoveContact={this.removeContact} /> )}     
+          {contacts.map(contact => <Contact key={contact.id} contact={contact} onRemoveContact={this.removeContact} /> )}
         </ol>
-      </div>    
+      </div>
     );
-  }  
+  }
 }
 
 export default App;
